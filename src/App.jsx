3e57bdf10eea533aef2c50722683f47575ce64f8
dymafr <contact@dyma.fr>
2023-11-02 23:51:53 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header/Header';
+import HomePage from './pages/HomePage/HomePage';
+import Footer from './components/Footer/Footer';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={`d-flex flex-column ${styles.appContainer}`}>
+      <Header />
+      <HomePage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
